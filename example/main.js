baaa07362b682069f434ed8f5838304a6e4876c8
billyct <billy_allen@126.com>
2019-06-05 21:03:01 +0800

    🐰Update example

---
+++ example/main.js
@@ -0,0 +1,13 @@
+import XiXisys from 'xixisys-js-sdk'
+
+// const sdk = XiXisys('')
+//
+// sdk.ComplianceHtml({
+//   id: "#compliance-html",
+//   cas: "115-07-1",
+// })
+
+XiXisys.ComplianceHtml('', {
+  id: "#compliance-html",
+  cas: "115-07-1",
+})
