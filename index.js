8733802e9968d724a65cdcfe986109255419219d
billyct <billy_allen@126.com>
2019-07-28 15:21:23 +0800

    🐰Add func SdsHtml

--- index.js
+++ index.js
@@ -48,6 +48,30 @@ class XiXisys {
     this.api = api(apiKey)
   }
 
+  SdsHtml({
+    id,
+    cas,
+    edition = 'ghs',
+    cssHref,
+    error = console.error
+  }) {
+    (async () => {
+      try {
+        const { data } = await this.api.get('v1/sds/html', {
+          searchParams: {
+            cas, edition
+          }
+        }).json()
+
+        this.createFrame(id, cssHref, data);
+        return this
+      } catch (e) {
+        let err = this.makeError(e)
+        error(err)
+      }
+    })()
+  }
+
   ComplianceHtml({
      id,
      cas,
@@ -60,52 +84,62 @@ class XiXisys {
           searchParams: { cas },
         }).json()
 
-        // create frame
-        const element = selector(id)
-
-        if (element) {
-          const frame = document.createElement('iframe')
-
-          // 通过 postMessage 让 iframe 100% height
-          window.addEventListener('message', e => {
-            const { xixisys: { height } } = e.data
-            if (height) {
-              frame.height = height
-            }
-          })
-
-          // 传递 cssHref 给 iframe
-          frame.onload = () => {
-            frame.contentWindow.postMessage({ xixisys: { cssHref } }, '*')
-          }
-
-          // 默认样式
-          frame.style = 'border:none;width:100%;'
-          frame.src = data
-          element.appendChild(frame)
-        }
+        this.createFrame(id, cssHref, data);
 
         return this
       } catch (e) {
-        let err = e
-        if (e instanceof HTTPError) {
-          if (e.response.status === 404) {
-            err = new CasNotFoundError()
-          } else {
-            const errJson = await e.response.json()
-            err = new ServerError(errJson.error)
-          }
-        }
+        let err = await this.makeError(e);
 
         error(err)
       }
     })()
+  }
+
+  async makeError(e) {
+    let err = e
+    if (e instanceof HTTPError) {
+      if (e.response.status === 404) {
+        err = new CasNotFoundError()
+      } else {
+        const errJson = await e.response.json()
+        err = new ServerError(errJson.error)
+      }
+    }
+    return err;
+  }
+
+  createFrame(id, cssHref, data) {
+    // create frame
+    const element = selector(id)
+
+    if (element) {
+      const frame = document.createElement('iframe')
+
+      // 通过 postMessage 让 iframe 100% height
+      window.addEventListener('message', e => {
+        const {xixisys: {height}} = e.data
+        if (height) {
+          frame.height = height
+        }
+      })
+
+      // 传递 cssHref 给 iframe
+      frame.onload = () => {
+        frame.contentWindow.postMessage({xixisys: {cssHref}}, '*')
+      }
+
+      // 默认样式
+      frame.style = 'border:none;width:100%;'
+      frame.src = data
+      element.appendChild(frame)
+    }
   }
 }
 
 const createInstance = () => {
   const sdk = apiKey => new XiXisys(apiKey)
   sdk.ComplianceHtml = (key, options) => createInstance()(key).ComplianceHtml(options)
+  sdk.SdsHtml = (key, options) => createInstance()(key).SdsHtml(options)
   return sdk
 }
 
