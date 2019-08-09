95d0fcf04457d305028e9961f1fe4f86fce9637f
billyct <billy_allen@126.com>
2019-08-09 18:37:53 +0800

    🐰Update example

--- example/main.js
+++ example/main.js
@@ -1,15 +1,21 @@
-import XiXisys from 'xixisys-js-sdk'
+import XiXisys from '../index'
 
 // api key for test
-const sdk = XiXisys('ivjvSXVHbz7Sk8R6tFLu36y3Gqvpv8AOa0OztMwM')
+const sdk = XiXisys(process.env.API_KEY)
 
 sdk.ComplianceHtml({
   id: '#compliance-html',
   cas: '115-07-1',
+  success: () => {
+    document.querySelector('#loading').remove()
+  }
 })
 
 sdk.SdsHtml({
   id: '#sds-html',
   cas: '110-46-3',
   edition: 'cn',
+  success: () => {
+    document.querySelector('#loading').remove()
+  }
 })
