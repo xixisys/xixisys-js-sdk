788f2a1a6d107de2f9c9adffc723d776af18586a
billyct <billy_allen@126.com>
2019-08-09 16:26:25 +0800

    🐰Update example

--- example/main.js
+++ example/main.js
@@ -1,15 +1,22 @@
 import XiXisys from 'xixisys-js-sdk'
 
 // api key for test
-const sdk = XiXisys('ivjvSXVHbz7Sk8R6tFLu36y3Gqvpv8AOa0OztMwM')
+const sdk = XiXisys(process.env.API_KEY)
 
 sdk.ComplianceHtml({
   id: '#compliance-html',
   cas: '115-07-1',
+  success: () => {
+    console.log('aka')
+    document.querySelector('#loading').remove()
+  }
 })
 
 sdk.SdsHtml({
   id: '#sds-html',
   cas: '110-46-3',
   edition: 'cn',
+  success: () => {
+    document.querySelector('#loading').remove()
+  }
 })
--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,9 +1,19 @@
+const webpack = require('webpack')
 const path = require('path')
 
-module.exports = {
-  entry: path.resolve(__dirname, 'main.js'),
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+module.exports = env => {
+  const apiKey = env.API_KEY || ''
+
+  return {
+    entry: path.resolve(__dirname, 'main.js'),
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+    },
+    plugins:[
+      new webpack.DefinePlugin({
+        'process.env.API_KEY': JSON.stringify(apiKey)
+      })
+    ]
   }
 }
